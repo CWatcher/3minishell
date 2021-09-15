c77f8bcf95609894a8b9e48f90b94874571c1326
Carmine Watcher <CWatcher@student.21-school.r>
2021-09-15 14:50:47 +0300

    main() += readline()

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,20 @@
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 16:38:32 by CWatcher          #+#    #+#             */
-/*   Updated: 2021/09/08 20:03:41 by CWatcher         ###   ########.fr       */
+/*   Updated: 2021/09/15 14:43:22 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <readline/readline.h>
 #include "parser/parser.h"
 
 int	main(void)
 {
-	parse("<< file echo some\\ arg \"ano'ther \"arg; 1||a && b");
-	parse("a|b");
-	parse("\"ls;\"");
+	char	*s;
+
+	s = readline("> ");
+	parse(s);
+	free(s);
 }
---
+++ tst.sh
@@ -0,0 +1,4 @@
+echo "" | ./minishell
+echo "a|b"  | ./minishell
+echo "\"ls;\"" | ./minishell
+echo "<< file echo some\\ arg \"ano'ther \"arg; 1||a && b" | ./minishell
