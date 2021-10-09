4b9832d30d1a9355aeb2219c509098ba057fe66f
Carmine Watcher <cwatcher@student.21-school.r>
2021-10-09 22:09:16 +0300

    run_builtin() += close(fd_backup)

--- srcs/pipex/run_builtin.c
+++ srcs/pipex/run_builtin.c
@@ -23,6 +23,18 @@ static int	fd_redirect(int fd, int target_fd)
 	return (fd_backup);
 }
 
+static int fd_restore(int fd, int target_fd)
+{
+	if (fd != target_fd)
+	{
+		if (fd >= 0 && dup2(target_fd, fd) != fd)
+			return (ft_perror("mish: fd_restore():", -1));
+		if (close(fd) != 0)
+			return (ft_perror("mish: fd_restore(): close()", -1));
+	}
+	return (fd);
+}
+
 int	run_builtin(t_builtin_func builtin_func, t_vector args, t_vector redirs,
 				t_vector *env)
 {
@@ -41,7 +53,7 @@ int	run_builtin(t_builtin_func builtin_func, t_vector args, t_vector redirs,
 	if (std_out_backup < 0)
 		return (-1);
 	r = builtin_func(args, env);
-	fd_redirect(STDIN_FILENO, std_in_backup);
-	fd_redirect(STDOUT_FILENO, std_out_backup);
+	fd_restore(std_in_backup, STDIN_FILENO);
+	fd_restore(std_out_backup, STDOUT_FILENO);
 	return (r);
 }
