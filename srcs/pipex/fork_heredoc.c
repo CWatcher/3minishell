ff79732094a5ff1d1f09865814605c76c6c89690
Carmine Watcher <cwatcher@student.21-school.r>
2021-10-11 20:52:09 +0300

    [buggy] +heredoc

--- srcs/pipex/fork_heredoc.c
+++ srcs/pipex/fork_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 17:04:17 by CWatcher          #+#    #+#             */
-/*   Updated: 2021/09/23 22:59:26 by CWatcher         ###   ########.fr       */
+/*   Updated: 2021/10/11 20:47:16 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 #include "ft_string.h"
 #include "ft_io.h"
 #include "exit_me.h"
+#include "pipex.h"
+#include "minishell.h"
 
-static void	get_put_heredoc(const char *limiter, int fd_out)
+static void	get_put_heredoc(char *limiter, int fd_out)
 {
 	char		*s;
 	int			r;
@@ -28,32 +30,37 @@ static void	get_put_heredoc(const char *limiter, int fd_out)
 		s = ft_free(s);
 		if (r < 0)
 			exit_me(ft_strdup("heredoc: failed to ft_putendl_fd()"));
-		if (ft_putstr("> ") < 0)
+		if (ft_putstr(">1 ") < 0)
 			exit_me(ft_strdup("heredoc: failed to ft_putstr()"));
 		r = ft_get_next_line(STDIN_FILENO, &s);
 	}
 	s = ft_free(s);
+	free(limiter);
 	if (close(fd_out) != 0)
 		exit_me(ft_strdup("Failed to close() in get_put_heredoc()"));
 	if (r < 0)
 		exit_me(ft_strdup("heredoc: failed to ft_get_next_line()"));
 }
 
-void	fork_heredoc(const char *limiter, int fd_out)
+t_bool	fork_heredoc(char *limiter, t_io_fds fds)
 {
 	pid_t	pid;
 
 	pid = fork();
 	if (pid == 0)
 	{
-		get_put_heredoc(limiter, fd_out);
+		if (close(fds.in) != 0)
+			ms_perror("child fork_heredoc()", "close(fds.in)", NULL, 1);
+		get_put_heredoc(limiter, fds.out);
 		exit_me(NULL);
 	}
-	else if (pid > 0)
+	else
 	{
-		if (close(fd_out) != 0)
-			exit_me(ft_strdup("Failed to close() in fork_heredoc()"));
+		free(limiter);
+		if (close(fds.out) != 0)
+			ms_perror("parent fork_heredoc()", "close(fds.out)", NULL, ft_true);
+		if (pid < 0)
+			return (ms_perror("fork_heredoc()", "fork()", NULL, ft_false));
 	}
-	else if (pid < 0)
-		exit_me(ft_strjoin("Failed to fork() on here_doc ", limiter));
+	return (ft_true);
 }
--- srcs/pipex/pipex.h
+++ srcs/pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 15:54:49 by CWatcher          #+#    #+#             */
-/*   Updated: 2021/10/11 16:26:44 by CWatcher         ###   ########.fr       */
+/*   Updated: 2021/10/11 20:45:39 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ typedef struct s_io_fds
 
 int		ft_perror(const char *s, int ret);
 pid_t	fork_cmd(t_vector args, t_vector redirs, t_vector *env, int fd_in, int fd_out);
+t_bool	fork_heredoc(char *limiter, t_io_fds pipe);
 int		run_builtin(t_builtin_func builtin_func, t_vector args, t_vector redirs,
 				t_vector *env);
 int		run_pipeline(t_vector pipeline, t_vector *env);
